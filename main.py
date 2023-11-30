13397b09bdaaaf6b9c76694331107cbff2d48d43
wtc0629 <wtc19990629@gmail.com>
2023-11-30 18:14:13 +0100

    All

--- main.py
+++ main.py
@@ -1,127 +1,110 @@
 import pandas as pd
-from sklearn.model_selection import KFold
-from tsfresh import extract_features, select_features
+from tsfresh import extract_features
 from tsfresh.utilities.dataframe_functions import impute
+from sklearn.model_selection import LeaveOneOut
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-gestures = ["horizontally", "horizontally fast", "vertically", "vertically fast", "near to far", "near to far fast",
-            "square", "square fast", "left circle (anticlockwise)", "right circle (clockwise)",
-            "large right circle (clockwise)"]
+tester_name = ["PHD", "DRP", "QXR", "TMZ", "TYT", "WTC", "WTC2", "ZC", "YKD", "WTC3", "Philipp", "Dennis","1","2","3","Felix"]
+#tester_name = ["PHD", "DRP", "QXR", "ZC", "WTC3", "Dennis", "1", "2", "3"]
+# tester_name = ["PHD", "QXR", "Philipp", "Dennis", "1", "3"]
+#tester_name = ["PHD", "QXR", "Philipp"]
+
+
+def loo_cross_validation(data_frames, new_y, y):
+    loo = LeaveOneOut()
+
+    accuracies = []
+    j = 0
+    for train_index, test_index in loo.split(data_frames):
+        train_data = pd.concat([data_frames[i] for i in train_index])
+        test_data = pd.concat([data_frames[i] for i in test_index])
+
+        # 从目标标签中选择对应的数据
+        y_train = new_y
+        y_test = y
+
+        # 训练和测试模型
+        classifier = RandomForestClassifier(max_features=100, random_state=5)
+        classifier.fit(train_data.values, y_train.squeeze())
+        predictions = classifier.predict(test_data)
+        # print(predictions)
+        # print(len(predictions))
+        # print("1")
+        cm = confusion_matrix(y_test, predictions)
+        # sns.heatmap(cm,
+        #            annot=True,
+        #            fmt='g')
+        # plt.ylabel('Prediction', fontsize=13)
+        # plt.xlabel('Actual', fontsize=13)
+        #cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[
+        #    "hor", "lrc", "lc", "ntf", "rc", "sq", "ver"])
+        cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[
+                     "fhor", "hor", "lrc", "lc", "fntf", "ntf", "rc", "fsq", "sq", "fver", "ver"])
+        cm_display.plot()
+        plt.title('Confusion Matrix', fontsize=17)
+        plt.savefig(f"C:\\Users\\51004\\Desktop\\MergeCSV\\{tester_name[j]}\\ConfusionMatrix.png")
+        plt.show()
+        j = j + 1
+
+        # 计算准确率
+        accuracy = accuracy_score(y_test.squeeze(), predictions)
+        accuracies.append(accuracy)
+
+    return accuracies
 
 
 if __name__ == "__main__":
-    y_index = ['horizontally fast_GestureTime0', 'horizontally fast_GestureTime1',
-               'horizontally fast_GestureTime2', 'horizontally fast_GestureTime3',
-               'horizontally fast_GestureTime4', 'horizontally fast_GestureTime5',
-               'horizontally fast_GestureTime6', 'horizontally fast_GestureTime7',
-               'horizontally_GestureTime0', 'horizontally_GestureTime1',
-               'horizontally_GestureTime2', 'horizontally_GestureTime3',
-               'horizontally_GestureTime4', 'horizontally_GestureTime5',
-               'horizontally_GestureTime6', 'horizontally_GestureTime7',
-               'large right circle (clockwise)_GestureTime0',
-               'large right circle (clockwise)_GestureTime1',
-               'large right circle (clockwise)_GestureTime2',
-               'large right circle (clockwise)_GestureTime3',
-               'large right circle (clockwise)_GestureTime4',
-               'large right circle (clockwise)_GestureTime5',
-               'large right circle (clockwise)_GestureTime6',
-               'large right circle (clockwise)_GestureTime7',
-               'left circle (anticlockwise)_GestureTime0',
-               'left circle (anticlockwise)_GestureTime1',
-               'left circle (anticlockwise)_GestureTime2',
-               'left circle (anticlockwise)_GestureTime3',
-               'left circle (anticlockwise)_GestureTime4',
-               'left circle (anticlockwise)_GestureTime5',
-               'left circle (anticlockwise)_GestureTime6',
-               'left circle (anticlockwise)_GestureTime7',
-               'near to far fast_GestureTime0', 'near to far fast_GestureTime1',
-               'near to far fast_GestureTime2', 'near to far fast_GestureTime3',
-               'near to far fast_GestureTime4', 'near to far fast_GestureTime5',
-               'near to far fast_GestureTime6', 'near to far fast_GestureTime7',
-               'near to far_GestureTime0', 'near to far_GestureTime1',
-               'near to far_GestureTime2', 'near to far_GestureTime3',
-               'near to far_GestureTime4', 'near to far_GestureTime5',
-               'near to far_GestureTime6', 'near to far_GestureTime7',
-               'right circle (clockwise)_GestureTime0',
-               'right circle (clockwise)_GestureTime1',
-               'right circle (clockwise)_GestureTime2',
-               'right circle (clockwise)_GestureTime3',
-               'right circle (clockwise)_GestureTime4',
-               'right circle (clockwise)_GestureTime5',
-               'right circle (clockwise)_GestureTime6',
-               'right circle (clockwise)_GestureTime7', 'square fast_GestureTime0',
-               'square fast_GestureTime1', 'square fast_GestureTime2',
-               'square fast_GestureTime3', 'square fast_GestureTime4',
-               'square fast_GestureTime5', 'square fast_GestureTime6',
-               'square fast_GestureTime7', 'square_GestureTime0',
-               'square_GestureTime1', 'square_GestureTime2', 'square_GestureTime3',
-               'square_GestureTime4', 'square_GestureTime5', 'square_GestureTime6',
-               'square_GestureTime7', 'vertically fast_GestureTime0',
-               'vertically fast_GestureTime1', 'vertically fast_GestureTime2',
-               'vertically fast_GestureTime3', 'vertically fast_GestureTime4',
-               'vertically fast_GestureTime5', 'vertically fast_GestureTime6',
-               'vertically fast_GestureTime7', 'vertically_GestureTime0',
-               'vertically_GestureTime1', 'vertically_GestureTime2',
-               'vertically_GestureTime3', 'vertically_GestureTime4',
-               'vertically_GestureTime5', 'vertically_GestureTime6',
-               'vertically_GestureTime7']
+
+    data_frames = []
+    # for i in range(15):
+    # for i in range(6):
+    # for i in range(3):
+    y_index = []
+    for i in range(len(tester_name)):
+        filename = f"C:\\Users\\51004\\Desktop\\MergeCSV\\{tester_name[i]}\\train_data.csv"
+        data = pd.read_csv(filename)
+        features = extract_features(data, column_id="process", column_sort="gaze_timestamp_datetime")
+        features = impute(features)
+        y_index = list(features.index)
+        data_frames.append(features)
+
+
 
     gestures2 = ["horizontally fast", "horizontally",
                  "large right circle (clockwise)", "left circle (anticlockwise)", "near to far fast",
                  "near to far", "right circle (clockwise)", "square fast",
                  "square", "vertically fast", "vertically"]
+    # gestures2 = ["horizontally", "horizontally",
+    #             "large right circle (clockwise)", "left circle (anticlockwise)", "near to far",
+    #            "near to far", "right circle (clockwise)", "square",
+    #             "square", "vertically", "vertically"]
     y_gestures = []
     for i in range(11):
-        for j in range(8):
+        for j in range(32):
             y_gestures.append(gestures2[i])
     y_data = pd.DataFrame({'gesture': y_gestures},
                           index=y_index)
     y = y_data.squeeze()
+
+    # print(y)
+    # print("0")
     new_y_dataframes = []
-    for i in range(11):
+    # for i in range(14):
+    # for i in range(5):
+    # for i in range(2):
+    for i in range(len(tester_name) - 1):
         new_y_dataframes.append(y_data)
     new_y_data = pd.concat(new_y_dataframes)
     new_y = new_y_data.squeeze()
 
-    tester_name = ["PHD", "DRP", "QXR", "TMZ", "TYT", "WTC", "WTC2", "ZC", "YKD", "WTC3", "Philipp", "Dennis"]
-    data_frames = []
+    accuracies = loo_cross_validation(data_frames, new_y, y)
 
-    for i in range(11):
-        filename = "C:\\Users\\51004\\Desktop\\MergeCSV\\" + tester_name[i] + "\\train_data.csv"
-        data = pd.read_csv(filename)
-        features = extract_features(data, column_id="process", column_sort="gaze_timestamp_datetime")
-        features = impute(features)
-        #features_filtered = select_features(features, y)
-        #features_filtered = impute(features_filtered)
-        data_frames.append(features)
-        #data_frames.append(features)
-
-    # 合并11个数据帧为一个训练数据帧
-    train_data = pd.concat(data_frames)
-    #train_data.fillna(0, inplace=True)
-    print(train_data)
-    train_data.to_csv("C:\\Users\\51004\\Desktop\\MergeCSV\\Dennis\\ex1.csv")
-    # 准备目标标签
-    # y_gestures = ["gesture_1", "gesture_2", ..., "gesture_11"]
-    # y_data = pd.DataFrame({'gesture': y_gestures})
-
-    # 训练随机森林分类器
-    classifier = RandomForestClassifier()
-    classifier.fit(train_data.values, new_y)
-
-    # 读取第12个gaze_merged.csv文件并提取特征值
-    test_data = pd.read_csv("C:\\Users\\51004\\Desktop\\MergeCSV\\" + tester_name[11] + "\\train_data.csv")
-    test_features = extract_features(test_data, column_id="process", column_sort="gaze_timestamp_datetime")
-    test_features = impute(test_features)
-    #test_features_filtered = select_features(test_features, y)
-    #test_features_filtered = impute(test_features_filtered)
-    #test_features_filtered.fillna(0, inplace=True)
-    #test_features.fillna(0, inplace=True)
-    # 使用训练好的分类器进行预测
-    predictions = classifier.predict(test_features)
-
-    # 验证准确率
-    # true_labels = ["true_gesture_1", "true_gesture_2", ..., "true_gesture_n"]  # 第12个文件的真实标签
-    accuracy = accuracy_score(y, predictions)
-    print(f"Accuracy: {accuracy}")
+    plt.close('all')
+    print(accuracies)
+    average_accuracy = sum(accuracies) / len(accuracies)
+    print(f"Average Accuracy: {average_accuracy}")
